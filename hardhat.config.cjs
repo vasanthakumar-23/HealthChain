84f9308ba696728762b84ea5fcc086793bf818a6
vasanthakumar <119485353+vasanthakumar-23@users.noreply.github.com>
2024-08-31 17:34:02 +0530

    Update hardhat.config.cjs

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -5,8 +5,6 @@ module.exports = {
   solidity: "0.8.24",
   networks:{
     sepolia:{
-    url:"https://eth-sepolia.g.alchemy.com/v2/7av629syRpB0ZOKWnOIcDIRqizIOu-Eg",
-    accounts:["4e74f7ec43f81499c3a30c5ba327be2eb8227673e2169896bd10884ced2b3ced"]
   },
     localhost:{}
   }
